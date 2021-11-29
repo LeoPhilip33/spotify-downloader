66a99784c860be046a9195d1b9899f0ff5b12711
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2021-11-29 19:37:41 +0100

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title:string = 'spotify-downloader';
+  inputEmptyHtml:string = "display: none;";
+
+  inputEmpty(){
+    console.log("L'input est vide")
+    this.inputEmptyHtml = "";
+  }
+
+  inputValue(valueUserInput:string){
+    if(valueUserInput == ""){
+      this.inputEmpty();
+    }
+    if(valueUserInput !== ""){
+      this.inputEmptyHtml = "display: none;";
+      console.log(valueUserInput)
+    }
+  }
+
+
+}
+
